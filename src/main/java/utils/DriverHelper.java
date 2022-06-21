9eb5121d047dfd75fb3a0f19c2f6408f08b2939c
realejandro <realejandrocf@gmail.com>
2022-06-20 21:45:17 -0500

    finishing DriverHelper

--- src/main/java/utils/DriverHelper.java
+++ src/main/java/utils/DriverHelper.java
@@ -1,4 +1,14 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+
 public class DriverHelper {
+    private static WebDriver driver;
+
+    private DriverHelper(){}
+
+    public static WebDriver getDriver(){
+        return driver;
+    }
+
 }
